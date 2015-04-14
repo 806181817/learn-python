aa08f77729f5da2fdcb7888b8f7a78512e221276
unknown <806181817@qq.com>
2015-04-14 18:43:24 +0800

    test2-3 file

---
+++ test2-3.py
@@ -0,0 +1,16 @@
+﻿#-*- coding:utf-8 -*- 
+
+######学生基本信息############ 
+#姓名： 赵岩
+#学号： 1403050227
+#班级： 通风14-2班
+
+#物理题：17.28 
+###########题目############### 
+import math
+
+i0=float(10.0)
+def function1(i):
+    i=float(i*math.cos(math.radians(30))**2)
+	return i
+print 'The solution is:'+str(function1(function1(function1(0.5*i0)))/float(0.5*i0)
