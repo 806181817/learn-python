04f429ac965f636f2ab316a8cfe5bed5bf75e63f
unknown <806181817@qq.com>
2015-05-31 16:22:25 +0800

    （1）增加了结束代码。 （2）改变来原来代码顺序，使得更加美观且合理。 （3）对重复的代码进行了删改。 请老师过目，谢谢老师。

---
+++ ID3.py
@@ -0,0 +1,43 @@
+﻿#!/usr/bin/env python
+# -*-  coding=utf-8 -*-
+
+chmap = {'0':0,'1':1,'2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9,'x':10,}
+
+def ch_to_num(ch):
+    return chmap[ch]
+	
+def verify_list(l):  
+    sum = 0  
+    for ii,n in enumerate(l):
+        i = 18-ii  
+        weight = 2**(i-1) % 11  
+        sum = (sum + n*weight) % 11  
+          
+#        print "i=%d,weight=%d,n=%d,sum=%d"%(i,weight,n,sum)  
+      
+#    print sum  
+    return sum==1
+  
+def verify_string(s):  
+    char_list = list(s)  
+    num_list = [ch_to_num(ch) for ch in char_list]  
+    return verify_list(num_list)
+  
+
+     
+if __name__=='__main__':
+    while True:
+        print '''---------------restart-------------------
+退出系统请输入exit
+校验身份证号码请直接输入身份证号码：'''
+        IDcard=raw_input()
+        if IDcard=='exit':
+            break
+        if len(str(IDcard))!=18:
+            print  "位数有错或没有输入" 
+        else:
+            result = verify_string(IDcard)  
+            if result:  
+               print "有效的身份证号码"  
+            else:  
+               print "错误的身份证号码"
